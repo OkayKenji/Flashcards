3358c36cef3e8bff944342de89b6268d6e72fed0
Kenji <70079754+OkayKenji@users.noreply.github.com>
2022-11-30 19:54:45 +0000

    dropdown menu

--- script.js
+++ script.js
@@ -9,20 +9,27 @@ function setup() {
   createCanvas(700, 200);
   let rows = table.rows;
    wordPairs = [];
+  sel = createSelect();
+  sel.position(150, 0);
+  let i = 0;
   for (let words of rows) {
     wordPairs.push([words.arr[0],words.arr[1]]);
+    sel.option(`${i}: ${words.arr[0]} | ${words.arr[1]}`);
+    i++;
   }
+  sel.changed(mySelectEvent);
   max = wordPairs.length;
   console.log(wordPairs);
   button = createButton('previous');
   button.position(0, 0);
   button.mousePressed(prev);
   button1 = createButton('flip');
-  button1.position(100, 0);
+  button1.position(70, 0);
   button1.mousePressed(flip);
   button2 = createButton('next');
-  button2.position(200, 0);
+  button2.position(105, 0);
   button2.mousePressed(next);
+  
   rectMode(CENTER);
 }
 
@@ -31,12 +38,12 @@ function draw() {
   noLoop();
   textSize(32);
   fill(255);
-  
+  printer();
 }
 
 function printer() {
   background(0);
-  text(wordPairs[index][bit], 20, height/2);
+  text(`${index}: ${wordPairs[index][bit]}`, 20, height/2);
 }
 
 function prev() {
@@ -62,4 +69,12 @@ function rangeCheck() {
   } else if (index < 0) {
     index = max - 1;
   }
+}
+
+function mySelectEvent() {
+  let item = sel.value().substring(0,sel.value().indexOf(':'));
+ //   charAt(0);
+  index = parseInt(item);
+  rangeCheck();
+  printer();
 }
