a2c2bb6bc10835f1942c4e38ee7fa1a945a0ca94
Kenji <70079754+OkayKenji@users.noreply.github.com>
2022-11-30 07:49:56 +0000

    final deploy

--- script.js
+++ script.js
@@ -1,15 +1,65 @@
 function preload() {
   table = loadTable('Chapter5Vocab.csv', 'csv', 'header');
 }
-
+let index = 0;
+let bit = 0;
+let max = -1;
+let wordPairs;
 function setup() {
+  createCanvas(700, 200);
   let rows = table.rows;
-  for (let words in rows) {
-    console.log(words.arr[0] + " " + words.arr[1]);
+   wordPairs = [];
+  for (let words of rows) {
+    wordPairs.push([words.arr[0],words.arr[1]]);
   }
-  createCanvas(400, 400);
+  max = wordPairs.length;
+  console.log(wordPairs);
+  button = createButton('previous');
+  button.position(0, 0);
+  button.mousePressed(prev);
+  button1 = createButton('flip');
+  button1.position(100, 0);
+  button1.mousePressed(flip);
+  button2 = createButton('next');
+  button2.position(200, 0);
+  button2.mousePressed(next);
+  rectMode(CENTER);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  noLoop();
+  textSize(32);
+  fill(255);
+  
+}
+
+function printer() {
+  background(0);
+  text(wordPairs[index][bit], 20, height/2);
+}
+
+function prev() {
+  index--;
+  rangeCheck();
+  printer();
+}
+function next() {
+  index++;
+  rangeCheck();
+  printer();
+}
+function flip() {
+  if (bit == 0) 
+    bit = 1;
+  else
+    bit = 0;
+  printer();
+}
+function rangeCheck() {
+  if (index >= max) {
+    index = 0;
+  } else if (index < 0) {
+    index = max - 1;
+  }
 }
